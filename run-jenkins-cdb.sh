0048df26bfd6e2f54eee52132b47e911e247a2a4
vincent BOCHET <vbochet@excilys.com>
2018-04-05 15:02:15 +0200

    Add Jenkins job config when building/running the Jenkins container

--- run-jenkins-cdb.sh
+++ run-jenkins-cdb.sh
@@ -4,3 +4,6 @@ sudo docker run \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $(which docker):/usr/bin/docker \
 	--name jenkins-cdb-instance jenkins-cdb
+
+sudo docker exec jenkins-cdb-instance sudo chown -R jenkins:jenkins /var/jenkins_home/jobs
+
